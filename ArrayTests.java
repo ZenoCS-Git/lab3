f5a9660632e906dd52a12b71df45de693fdf61bb
ZenoCS-Git <zenothemasterofcats@gmail.com>
2023-01-26 14:32:36 -0800

    fixed methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,12 +7,18 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = { 3, 2, 5 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 5, 2, 3 }, input2);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = { 1, 2, 3};
+    assertArrayEquals(new int[]{ 3, 2, 1}, ArrayExamples.reversed(input1));
+
+
   }
 }
